eca735f7063fede93af6853e4ebb4f25e7528365
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2025-05-12 13:50:21 +0300

    Scenario4 appstore malware (#16)
    * Added initial configs
    
    * Added store apps
    
    * fixvug: delte uneccessary emthod
    
    ---------
    
    Co-authored-by: rkibistu <61421173+rkibistu@users.noreply.github.com>

---
+++ Assets/Scripts/4_Scenario_WebPage/StoreApp.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScenarioSQL {
+
+    public class StoreApp : MonoBehaviour {
+        [SerializeField]
+        private TextMeshProUGUI _titleText;
+        [SerializeField]
+        private TextMeshProUGUI _priceText;
+        [SerializeField]
+        private Button _buyButton;
+
+        public string TitleText {
+            get { return _titleText.text; }
+            set { _titleText.text = value; }
+        }
+        public string Price {
+            get { return _priceText.text; }
+            set { _priceText.text = value; }
+        }
+    }
+
+}
