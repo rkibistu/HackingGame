a281e7136f046fe3f8cd0248e2072e7b99f2d0ca
Teodor Amzuloiu <amzteodor@gmail.com>
2025-05-16 16:10:57 +0300

    Browser opener

---
+++ Assets/Scripts/BrowserOpener/BrowserOpener.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class BrowserOpener : MonoBehaviour
+{
+    [Header("Open Browser")]
+    [SerializeField]
+    private string url = "https://your-url.com";
+
+    [SerializeField]
+    private int width = 800;
+    [SerializeField]
+    private int height = 600;
+    [SerializeField]
+    private int x = 100;
+    [SerializeField] 
+    private int y = 100;
+
+    public void OpenBrowser()
+    {
+        string args = $"--new-window \"{url}\" --window-size={width},{height} --window-position={x},{y}";
+
+        // Try Chrome
+        if (TryStart("chrome", args)) return;
+
+        // Try Edge
+        if (TryStart("msedge", args)) return;
+
+        // Try Firefox (note: limited support for window position)
+        if (TryStart("firefox", $"-new-window \"{url}\"")) return;
+
+        // Fallback: open in default browser
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+
+    private bool TryStart(string browser, string args)
+    {
+        try
+        {
+            Process.Start(browser, args);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
