4ba20e363d58978c3476b872490c2d9c4182a3c0
Ogrean Razvan <ogreanrazvan@gmail.com>
2025-05-17 13:49:59 +0300

    audiomixer to control sound volume

--- Assets/Scripts/Audio/AudioControllerBasic.cs
+++ Assets/Scripts/Audio/AudioControllerBasic.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 
 
 [System.Serializable]
@@ -18,6 +19,12 @@ public class AudioControllerBasic : MonoBehaviour
 {
     [Header("Add Audio Clips Here")]
     public List<NamedAudioClip> _audioClips = new List<NamedAudioClip>();
+
+    [Header("Settings")]
+    [SerializeField]
+    private AudioMixer _audioMixer;
+    [SerializeField]
+    private AudioMixerGroup _sfxMixerGroup;
 
     private Dictionary<string, NamedAudioClip> _audioClipDict = new Dictionary<string, NamedAudioClip>();
 
@@ -41,13 +48,49 @@ public class AudioControllerBasic : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SetSFXVolume(0.2f);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SetSFXVolume(1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            PlaySound("knock-door");
+        }
+    }
+
     public void PlaySound(string clipName) {
         if (_audioClipDict.TryGetValue(clipName, out NamedAudioClip namedClip)) {
             Vector3 position = namedClip.playLocation != null ? namedClip.playLocation.position : transform.position;
-            AudioSource.PlayClipAtPoint(namedClip.clip, position, namedClip.volume);
+            //AudioSource.PlayClipAtPoint(namedClip.clip, position, namedClip.volume);
+
+            GameObject tempGO = new GameObject("TempAudio_" + clipName);
+            tempGO.transform.position = position;
+
+            AudioSource aSource = tempGO.AddComponent<AudioSource>();
+            aSource.clip = namedClip.clip;
+            aSource.volume = namedClip.volume;
+            aSource.outputAudioMixerGroup = _sfxMixerGroup;
+            aSource.spatialBlend = 0f; // Make it 2D if needed
+            aSource.Play();
+
+            Destroy(tempGO, namedClip.clip.length + 0.1f); // Clean up after playback
         }
         else {
             Debug.LogWarning("Sound not found: " + clipName);
         }
+    }
+
+
+
+    public void SetSFXVolume(float volume01)
+    {
+        float dB = Mathf.Log10(Mathf.Clamp(volume01, 0.0001f, 1f)) * 20f;
+        _audioMixer.SetFloat("SFXVolume", dB);
     }
 }
