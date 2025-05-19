f3ae2279a97b30598aaca0f9302015cff1db8fcd
Teodor Amzuloiu <amzteodor@gmail.com>
2025-05-19 10:30:18 +0300

    Scenario 2 update

--- Assets/Scripts/2_ScenarioForkBomb/LoginManager.cs
+++ Assets/Scripts/2_ScenarioForkBomb/LoginManager.cs
@@ -28,37 +28,57 @@ public class LoginManager : MonoBehaviour
     [Header("Login story and task IDs")]
     [SerializeField]
     private string _taskAfterSuccessLoginPageId;
+    [SerializeField]
+    private string _tastCurrentAllowedLoginPageId;
 
     void Start()
     {
         //_loginButton.onClick.AddListener(OnLoginButtonClicked);
         _errorLoginText.gameObject.SetActive(false);
+        _errorLoginText.text = "Password or username incorrect!";
         _submitWebpage.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _errorLoginText.gameObject.SetActive(false);
     }
 
     public void OnLoginButtonClicked()
     {
-        string enteredUsername = _usernameInputField.text;
-        string enteredPassword = _passwordInputField.text;
-
-        if (enteredUsername == correctUsername && enteredPassword == correctPassword)
+        if (TasksController.Instance.CheckCurrentTask(_tastCurrentAllowedLoginPageId))
         {
-            TasksController.Instance.ActivateTask(_taskAfterSuccessLoginPageId);
-            ClearPanel();
-            _loginWebpage.SetActive(false);
-            _submitWebpage.SetActive(true);
-            _errorLoginText.gameObject.SetActive(false); // Disable error text if it was previously active.   
+
+
+            string enteredUsername = _usernameInputField.text;
+            string enteredPassword = _passwordInputField.text;
+
+            if (enteredUsername == correctUsername && enteredPassword == correctPassword)
+            {
+                TasksController.Instance.ActivateTask(_taskAfterSuccessLoginPageId);
+                ClearPanel();
+                _loginWebpage.SetActive(false);
+                _submitWebpage.SetActive(true);
+                _errorLoginText.text = "Password or username incorrect!";
+                _errorLoginText.gameObject.SetActive(false); // Disable error text if it was previously active.   
+            }
+            else
+            {
+                _errorLoginText.text = "Password or username incorrect!";
+                _errorLoginText.gameObject.SetActive(true);
+            }
         }
         else
         {
+            _errorLoginText.text = "Check the credentials file first!";
             _errorLoginText.gameObject.SetActive(true);
         }
     }
 
     public void OnLogoutButtonClicked()
     {
-            _loginWebpage.SetActive(true);
-            _submitWebpage.SetActive(false);
+        _loginWebpage.SetActive(true);
+        _submitWebpage.SetActive(false);
     }
 
     private void ClearPanel()
